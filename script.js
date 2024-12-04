d20d7360746a2c9540534e7487ab9b72bf1f60c2
Sean Cho <137361461+seanncho@users.noreply.github.com>
2024-12-04 17:12:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+document.addEventListener("click", function(event) {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  const target = event.target;
+
+  if (!menu.contains(target) && !icon.contains(target)) {
+    menu.classList.remove("open");
+    icon.classList.remove("open");
+  }
+});
+
+const elementsToFadeInUpOnScroll = document.querySelectorAll(".tag");
+if (elementsToFadeInUpOnScroll) {
+  window.addEventListener("scroll", function(event) {
+    elementsToFadeInUpOnScroll.forEach(function(element) {
+      // Get the position of the element relative to the viewport
+      const elementTop = element.getBoundingClientRect().top;
+      const elementBottom = element.getBoundingClientRect().bottom;
+      const windowHeight = window.innerHeight;
+
+      // Check if the element is in the viewport
+      if (elementTop < windowHeight * 0.7 && elementBottom > 0) {
+        element.classList.add("fade-in-up");
+        element.classList.remove("fade-out");
+      } else {
+        element.classList.remove("fade-in-up");
+        element.classList.add("fade-out");
+      }
+    });
+  });
+}
